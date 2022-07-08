56f45f6d61f2b1f488c93e48f144de3c5d8a6e8c
jeff.park <jinopark1119@gmail.com>
2022-07-09 02:56:09 +0900

    home image added

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="Background" src="img/Home.jpg" />
+      <div className="TopBar">
+        <h1 className="Title">Minap</h1>
+        <img className="AddButton" src="img/AddButton.png" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
